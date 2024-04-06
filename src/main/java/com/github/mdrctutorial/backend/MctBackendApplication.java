c67d8c2140c36eb0e2ca953485bb448dbb3c5c16
Minsecrus <2972853299@qq.com>
2024-04-06 08:09:23 +0800

    init

---
+++ src/main/java/com/github/mdrctutorial/backend/MctBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.mdrctutorial.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MctBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MctBackendApplication.class, args);
+    }
+
+}
